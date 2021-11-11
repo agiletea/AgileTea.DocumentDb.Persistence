4c4a3a81f997809ea367e8de3916068694169487
Benjamin Jones <ben@agiletea.com>
2021-11-11 16:35:47 +0000

    Changed record classes to be records and fixed unit tests

--- src/AgileTea.Persistence.Common/Records/IndexedRecordBase.cs
+++ src/AgileTea.Persistence.Common/Records/IndexedRecordBase.cs
@@ -4,7 +4,7 @@ using AgileTea.Persistence.Common.Interfaces;
 namespace AgileTea.Persistence.Common.Records
 {
     [ExcludeFromCodeCoverage]
-    public abstract class IndexedRecordBase<T> : IIndexedEntity<T>
+    public abstract record IndexedRecordBase<T> : IIndexedEntity<T>
     {
         public virtual T Id { get; }
     }
